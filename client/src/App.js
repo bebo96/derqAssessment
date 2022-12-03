bfede27b444bdb3ea149489e538a87fc7e93032d
bebo <ibrahimimran96@gmail.com>
2022-12-02 19:33:00 -0500

    Add React app, required dependencies, and Navbar

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import NavScrollExample from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+     <div>
+       <NavScrollExample />
+     </div>
+)}
+
+export default App
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavScrollExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+        
+      <Container fluid>
+          <Form className="d-flex align-center">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
